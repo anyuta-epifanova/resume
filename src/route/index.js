20a2dbf81b45110c123778f88b44dbe3d3a7e131
Anet <epifanovaanyutaa@gmail.com>
2023-09-25 19:15:05 +0300

    lesson 2 bootstrap

--- src/route/index.js
+++ src/route/index.js
@@ -767,5 +767,14 @@ router.get('/js', function (req, res) {
 
 // ================================================================
 
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+})
+
 // Підключаємо роутер до бек-енду
 module.exports = router
